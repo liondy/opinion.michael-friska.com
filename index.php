cf6f9a42989dd602a69936a1509c2989524200b5
liondy <michaelliondy05@gmail.com>
2021-07-30 19:01:42 +0700

    add table opinion because of the damn cache

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php
 
 include_once 'storage/store.php';
+include_once 'functions/function.php';
 
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
 
 // echo 'ok';
 // echo "<br/>";
 // echo $_SERVER['REQUEST_METHOD'];
 // echo "<br/>";
+$servername = SERVERNAME;
+$username = USERNAME;
+$password = PASSWORD;
+$dbname = DB_NAME;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
@@ -17,13 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   // echo "<br/>";
   // echo $opinion;
-  
-  $servername = SERVERNAME;
-  $username = USERNAME;
-  $password = PASSWORD;
-  $dbname = DB_NAME;
-  
-  $conn = new mysqli($servername, $username, $password, $dbname);
 
   $name = mysqli_real_escape_string($conn, $_POST['testi-name']);
   $opinion = mysqli_real_escape_string($conn, $_POST['testi-opinion']);
@@ -45,44 +45,76 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
+} else {
+  $query = "SELECT * FROM `opinion` ORDER BY `id` DESC";
 
-  $conn->close();
-}
-else {
+  $result = $conn->query($query);
+
+  $i = 1;
 ?>
-<!DOCTYPE html>
-<html lang="en">
+  <!DOCTYPE html>
+  <html lang="en">
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Michael-Friska</title>
+    <title>Michael-Friska</title>
 
-	<!-- Google font -->
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
 
-	<!-- Custom stlylesheet -->
-	<link type="text/css" rel="stylesheet" href="css/style.css" />
+    <!-- Custom stlylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
 
-</head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<body>
+  </head>
 
-	<div id="notfound">
-		<div class="notfound">
-			<div class="notfound-404">
-				<h1>404</h1>
-			</div>
-			<h2>Oops! This Page Could Not Be Found</h2>
-			<p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
-			<a title="Michael-Friska" href="https://michael-friska.com">Go To Homepage</a>
-		</div>
-	</div>
+  <body>
+    <div class="container-md mt-5">
+      <header class="header">
+        <h1>Your Opinion</h1>
+        <p>Please check whether your opinion has been inserted into our database. Your opinion should be on the first row.</p>
+      </header>
+      <section class="table-responsive">
+        <table class="table table-hover table-striped align-middle">
+          <thead class="table-light">
+            <tr>
+              <th>No.</th>
+              <th>Name</th>
+              <th>Opinion</th>
+              <th>Rating</th>
+              <th>Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($result as $row) : ?>
+              <tr>
+                <td><?= $i++; ?></td>
+                <td><?= cutStrings($row['nama'],100); ?></td>
+                <td><?= cutStrings($row['opini'],100); ?></td>
+                <td><?= $row['rate']; ?></td>
+                <td><?= $row['timestamp']; ?></td>
+              </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+      </section>
+      <section>
+        <div class="mt-5">
+          <a type="button" class="btn btn-outline-info btn-lg" title="Michael-Friska" href="https://michael-friska.com">Back To Homepage</a>
+        </div>
+      </section>
+    </div>
+  </body>
 
-</body>
+  </html>
+<?php
+}
 
-</html>
-<?php } ?>
+$conn->close();
+?>
